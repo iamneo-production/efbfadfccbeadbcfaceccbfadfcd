c96fc47d4121efc048402caae6b1fc71d1f30f1a
Iamneo <techadmin@iamneo.ai>
2023-08-08 15:05:18 +0000

    Backup Commit

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -25,7 +25,7 @@ export class AppComponent {
     }
     else
     {
-      this.show = false;
+      this.show = true;
     }
   }
 }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CurrencyconvPipe } from './currencyconv.pipe';
 
 @NgModule({
@@ -15,8 +15,8 @@ import { CurrencyconvPipe } from './currencyconv.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    NgbModule
+    FormsModule//,
+    //NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
